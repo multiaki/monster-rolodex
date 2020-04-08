3f18d8282fe2b75fa18a064020eea56ddc510b0a
Akmal Sharipov <asharipov-nonemp@collegeboard.org>
2020-04-08 15:38:30 -0400

    Updates

---
+++ precache-manifest.9252d04aebf3c8fd6872c978fc35b1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27957af5c98b7f84ba66fbc50212ddfd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f79f4d380ab0bdd7479e",
+    "url": "/monster-rolodex/static/css/main.f0b6c1bd.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79f4d380ab0bdd7479e",
+    "url": "/monster-rolodex/static/js/main.2839be5f.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
